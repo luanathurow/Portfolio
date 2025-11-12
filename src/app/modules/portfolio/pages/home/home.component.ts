ac746148499b1af7bd6ccd50182184b5ef0ea067
Luana_Thurow <luana.thurow@dell.com>
2025-11-12 15:59:49 -0300

    Criando header

--- src/app/modules/portfolio/pages/home/home.component.ts
+++ src/app/modules/portfolio/pages/home/home.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 
+// Components
+import { HeaderComponent } from '../../components/header/header.component';
+import { KnowledgeComponent } from '../../components/knowledge/knowledge.component';
+
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [HeaderComponent, KnowledgeComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
